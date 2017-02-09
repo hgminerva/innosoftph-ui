cdf35dc7247d9297bfe02c68648b48a6b35f9589
NoahOliverRigonan <oliverrigonan@gmail.com>
2017-02-09 14:38:24 +0800

    git ignore node modules

--- node_modules/wijmo/wijmo.angular2.chart.hierarchical.d.ts
+++
@@ -1,9 +0,0 @@
-import * as wjcChartHierarchical from 'wijmo/wijmo.chart.hierarchical';
-import { Injector, ElementRef } from '@angular/core';
-export declare class WjSunburst extends wjcChartHierarchical.Sunburst {
-    constructor(elRef: ElementRef, injector: Injector);
-    tooltipContent: any;
-    labelContent: any;
-}
-export declare class WjChartHierarchicalModule {
-}
