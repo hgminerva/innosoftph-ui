52c8512e4f2d046f20c7773ad03aa5f6bd5e7b92
NoahOliverRigonan <oliverrigonan@gmail.com>
2017-02-09 18:11:10 +0800

    wijmo changes and package config modification

--- wijmo/wijmo.angular2.grid.grouppanel.d.ts
+++
@@ -1,7 +0,0 @@
-import * as wjcGridGrouppanel from 'wijmo/wijmo.grid.grouppanel';
-import { Injector, ElementRef } from '@angular/core';
-export declare class WjGroupPanel extends wjcGridGrouppanel.GroupPanel {
-    constructor(elRef: ElementRef, injector: Injector);
-}
-export declare class WjGridGrouppanelModule {
-}
